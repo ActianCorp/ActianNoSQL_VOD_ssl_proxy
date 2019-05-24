16194eafe434194a3884adf9782334fa23cbb7ec
Alberico Perrella Neto <aperrell@dehh-sup-argon.versant.com>
2019-05-24 16:10:01 +0200

    SSL client: get sure stunnel is installed

--- stunnel_install_pre_requisites.sh
+++ stunnel_install_pre_requisites.sh
@@ -12,24 +12,40 @@
 echo PATH=${PATH}
 STUNNEL=`which stunnel`;
 LIBWRAP=`ldd $stunnel | grep libwrap`;
-if [ -z $LIBWRAP ]
+if [ -z $LIBWRAP ] || [  -z $DSTUNNEL ]
   then
    echo "sudo yum install openssl stunnel"
    sudo yum install openssl stunnel
 fi
 
 # get sure ip6tables is installed and ready to be used
-echo "sudo yum install iptables-services"
-sudo yum install iptables-services
+echo " IPTABLES=`sudo which ip6tables`;"
+IPTABLES=`sudo which ip6tables`;
+if [ -z $IPTABLES ]
+ then
+	echo "sudo yum install iptables-services"
+	sudo yum install iptables-services
+ fi
 
 # if firewalld is active, then it overcomes iptables and ip6tables rules 
-echo "sudo systemctl stop firewalld "
-sudo systemctl stop firewalld 
-echo "sudo systemctl start ip6tables"
-sudo systemctl start ip6tables
-echo "sudo systemctl start iptables"
-sudo systemctl start iptables
-
-
+  SYSTEMCTL="/usr/bin/systemctl"
+  SERVICE="/sbin/service"
+if [ -e $SYSTEMCTL ]
+  then
+	echo "sudo $SYSTEMCTL stop firewalld "
+	sudo $SYSTEMCTL stop firewalld 
+	echo "sudo $SYSTEMCTL start ip6tables"
+	sudo $SYSTEMCTL start ip6tables
+	echo "sudo $SYSTEMCTL start iptables"
+	sudo $SYSTEMCTL start iptables
+ elif [ -e $SERVICE ]
+  then
+	echo "sudo $SERVICE firewalld stop"
+	sudo $SERVICE  firewalld stop 
+	echo "sudo $SERVICE  ip6tables start"
+	sudo $SERVICE  ip6tables start
+	echo "sudo $SERVICE  iptables start"
+	sudo $SERVICE iptables start
+fi
 #sudo /sbin/chkconfig  iptables on
 #sudo /sbin/chkconfig  ip6tables on
